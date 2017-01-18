2d02d37a45c94cd44a62b5666a5d99a050285493
josephprasanth <prasanthjoseph_77@yahoo.com>
2017-01-18 13:15:24 +0100

    fixed a basic login page - Styling left to do - Database is local

--- login/index.php
+++ login/index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,12 +10,43 @@
   </head>
   <body>
 
+<form action="login.php" method="POST">
+
+  <input type="text" name="uid" placeholder="Username"><br>
+  <input type="password" name="pwd" placeholder="Password"><br>
+  <button type="SUBMIT">LOGIN</button>
+
+</form>
+
+<?php
+if (isset($_SESSION['id'])) {
+echo $_SESSION['id'];
+}
+
+else {
+echo "You are not logged in";
+
+
+}
+
+
+ ?>
+
+<br><br><br>
+
 <form action="signup.php" method="POST">
 <input type="text" name="first" placeholder="FirstName"><br>
 <input type="text" name="last" placeholder="LastName"><br>
 <input type="text" name="uid" placeholder="Username"><br>
 <input type="password" name="pwd" placeholder="Password"><br>
 <button type="SUBMIT">SIGN UP</button>
+
+</form>
+
+<br><br><br>
+
+<form action="logout.php">
+<button> LOG OUT</button>
 
 </form>
   </body>
--- login/signup.php
+++ login/signup.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'dbh.php';
 
 $first = $_POST['first'];
@@ -7,8 +8,13 @@ $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
 
-echo $first."<br>";
-echo $last."<br>";
-echo $uid."<br>";
-echo $pwd."<br>";
+$sql = "INSERT INTO  user (first, last, uid, pwd)
+VALUES ('$first', '$last', '$uid', '$pwd') ";
+$result = $conn->query($sql);
+
+header("Location: index.php");
+
+
+
+
  ?>
