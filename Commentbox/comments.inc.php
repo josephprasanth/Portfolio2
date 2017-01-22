65d979fedb7dbcb76bba1e2086dce267943d7991
josephprasanth <prasanthjoseph_77@yahoo.com>
2017-01-22 14:56:10 +0100

    edit function in comment box is now working

--- Commentbox/comments.inc.php
+++ Commentbox/comments.inc.php
@@ -1,13 +1,10 @@
 <?php
 
 function setComments($conn) {
-
 if (isset($_POST['commentSubmit'])) {
-
   $uid = $_POST['uid'];
   $date = $_POST['date'];
   $message = $_POST['message'];
-
 $sql = "INSERT INTO comments (uid, date, message) VALUES ('$uid', '$date', '$message')";
 $result = mysqli_query($conn,$sql);
 //$result = $conn->query($sql);
@@ -17,18 +14,40 @@ $result = mysqli_query($conn,$sql);
 function getComments($conn) {
 $sql = "SELECT * FROM comments";
 $result = mysqli_query($conn,$sql);
-
 while($row = $result->fetch_assoc()) {
-
 echo "<div class='comment-box'><p>";
 echo $row['uid']."<br>";
 echo $row['date']."<br>";
 echo $row['message'];
-echo"</p></div>";
+echo"</p>
+<form class='edit-form' method='POST' action='editcomment.php'>
+<input type='hidden' name='cid' value='".$row['cid']."'>
+<input type='hidden' name='uid' value='".$row['uid']."'>
+<input type='hidden' name='date' value='".$row['date']."'>
+<input type='hidden' name='message' value='".$row['message']."'>
+<button >Edit</button>
+</form>
+
+
+</div>";
 
 }
 }
 
+
+
+function editComments($conn) {
+if (isset($_POST['commentSubmit'])) {
+  $cid = $_POST['cid'];
+  $uid = $_POST['uid'];
+  $date = $_POST['date'];
+  $message = $_POST['message'];
+$sql = "UPDATE comments SET message='$message' WHERE cid='$cid'";
+$result = mysqli_query($conn,$sql);
+header("Location: index.php");
+//$result = $conn->query($sql);
+}
+}
 
 
 
